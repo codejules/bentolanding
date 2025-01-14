6ee3d16a2a2d89cb83ee523b15149dcbfc1bda42
Julio Serra <julioserra@protonmail.com>
2025-01-14 10:21:28 +0100

    update bg image url

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -4,7 +4,7 @@ export default {
 	theme: {
 		extend: {
 			backgroundImage: {
-				'hero-pattern': "url('./src/assets/home-background.webp')",
+				'hero-pattern': "url('./home-background.webp')",
 			}
 		},
 	},
